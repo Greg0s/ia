3a512ca53e1c93a403c50babefd81d49ff6dd21a
Greg0s <gregoire.tinnes@outlook.fr>
2023-10-09 01:25:38 +0200

    fitness landscape update

--- tp4-genetic-algorithm/main.js
+++ tp4-genetic-algorithm/main.js
@@ -1,8 +1,7 @@
 import * as THREE from "three";
-import Stats from "three/examples/jsm/libs/stats.module";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { GUI } from "dat.gui";
 
-// const camera = initCamera();
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
   75,
@@ -10,53 +9,103 @@ const camera = new THREE.PerspectiveCamera(
   0.1,
   1000
 );
-camera.position.z = 8;
-camera.position.y = 5;
+camera.position.x = 16;
+camera.position.z = 16;
+camera.position.y = 12;
 
-const pointLight = new THREE.PointLight(0xff0000, 1, 10); // Couleur rouge, intensité 1, distance d'atteinte de 10 unités
-pointLight.position.set(0.1, 0, 15); // Position de la lumière
+const colors = [];
+
+const pointLight = new THREE.PointLight(0xff0000, 1, 10); // Red color, intensity 1, reach distance of 10 units
+pointLight.position.set(0.1, 0, 15); // Light position
 scene.add(pointLight);
+
+let width = window.innerWidth;
+let height = window.innerHeight;
+
+let popSize = 400;
+let generations = 200;
+let mutationRate = 0.1;
+
+let materialFacesColor = 0x0f0fff;
+let targetValue = 0.5;
+
+const chromosomeHTML = document.querySelector(".chromosome");
+const fitnessHTML = document.querySelector(".fitness");
+const targetValueHTML = document.querySelector(".target-value");
+
+// GUI
+const gui = new GUI();
+const params = {
+  color: 0x0f0fff,
+  reload: () => reload(),
+  popSize: 400,
+  generations: 200,
+  mutationRate: 0.1,
+  targetValue: 0.5,
+};
+const fitnessLandscape = gui.addFolder("Fitness landscape");
+fitnessLandscape.open();
+gui.add(params, "reload");
+fitnessLandscape
+  .add(params, "popSize", 300, 2000, 50)
+  .listen()
+  .onChange(function (value) {
+    popSize = value; // Update the variable when the GUI slider changes
+  })
+  .name("Population size");
+fitnessLandscape
+  .add(params, "generations", 100, 2000, 50)
+  .listen()
+  .onChange(function (value) {
+    generations = value;
+  })
+  .name("Generations");
+fitnessLandscape
+  .add(params, "mutationRate", 0.1, 1, 0.1)
+  .listen()
+  .onChange(function (value) {
+    mutationRate = value;
+  })
+  .name("Mutation rate");
+fitnessLandscape
+  .add(params, "targetValue", 0, 10, 0.5)
+  .listen()
+  .onChange(function (value) {
+    targetValue = value;
+    targetValueHTML.innerHTML = targetValue;
+  })
+  .name("Target value");
+fitnessLandscape
+  .addColor(params, "color")
+  .onChange(function (newColor) {
+    materialFacesColor = newColor;
+  })
+  .name("Color");
+
+function clearScene() {
+  while (scene.children.length > 0) {
+    const child = scene.children[0];
+    scene.remove(child);
+  }
+  // Réinitialiser les tableaux de données
+  vertices.fill(0); // Réinitialiser les données de vertices
+  colors.length = 0; // Réinitialiser le tableau de couleurs
+}
+
+function reload() {
+  clearScene();
+  targetValueHTML.innerHTML = targetValue;
+  geneticAlgorithmWithAdaptiveLandscape(popSize, generations, mutationRate);
+}
 
 (function init() {
   window.addEventListener("resize", onResize, false);
-  const stats = initStats();
 
   const renderer = initRenderer({
     antialias: true,
   });
 
   renderer.setSize(window.innerWidth, window.innerHeight);
-
-  // Lights
-  // let spotLight = new THREE.DirectionalLight();
-  // spotLight.position.set(-20, 250, -50);
-  // spotLight.target.position.set(30, -40, -20);
-  // spotLight.intensity = 0.3;
-  // scene.add(spotLight);
-
-  let spotLight = new THREE.DirectionalLight();
-  spotLight.position.set(-20, 250, -50);
-  spotLight.target.position.set(30, -40, -20);
-  spotLight.intensity = 0.3;
-
-  // Activer la projection des ombres pour la lumière
-  spotLight.castShadow = true;
-
-  scene.add(spotLight);
-
-  // Créez une lumière directionnelle avec des ombres
-  const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-  directionalLight.position.set(0, 10, 0); // Position de la lumière
-  directionalLight.castShadow = true; // Activez la projection des ombres
-
-  // Configurez les propriétés d'ombre
-  directionalLight.shadow.mapSize.width = 1024; // Largeur de la carte d'ombres
-  directionalLight.shadow.mapSize.height = 1024; // Hauteur de la carte d'ombres
-  directionalLight.shadow.camera.near = 0.5; // Distance proche de la caméra d'ombres
-  directionalLight.shadow.camera.far = 50; // Distance loin de la caméra d'ombres
-
-  // Ajoutez la lumière à la scène
-  scene.add(directionalLight);
 
   // OrbitControls initialization
   const controls = new OrbitControls(camera, renderer.domElement);
@@ -67,17 +116,8 @@ scene.add(pointLight);
   render();
 
   function render() {
-    stats.update();
     requestAnimationFrame(render);
     renderer.render(scene, camera);
-  }
-
-  function initStats() {
-    var stats = new Stats();
-    stats.setMode(0);
-
-    document.getElementById("stats").appendChild(stats.domElement);
-    return stats;
   }
 
   function onResize() {
@@ -108,6 +148,9 @@ function initRenderer(additionalProperties) {
   return renderer;
 }
 
+const gridSize = 20; // Number of grid points per side
+const vertices = new Float32Array(gridSize * gridSize * 3);
+
 /**
  * Camera initialization function
  *
@@ -115,21 +158,18 @@ function initRenderer(additionalProperties) {
  */
 
 function displayFitnessMap(population) {
-  const gridSize = 10; // Nombre de points de la grille par côté
-  const gridStep = 1; // Espacement entre les points de la grille
+  const gridStep = 1; // Spacing between grid points
 
   const geometry = new THREE.BufferGeometry();
-  const vertices = new Float32Array(gridSize * gridSize * 3);
+  // const vertices = new Float32Array(gridSize * gridSize * 3);
 
   for (let i = 0, x = -gridSize / 2; i < gridSize; i++, x += gridStep) {
     for (let j = 0, z = -gridSize / 2; j < gridSize; j++, z += gridStep) {
-      // Vérifiez si l'index est valide avant d'accéder à l'élément de population
+      // Check if the index is valid before accessing the population element
       if (i * gridSize + j < population.length) {
-        // console.log(population[i * gridSize + j]);
-        const fitness = population[i * gridSize + j].fitness * 0.15;
+        const fitness = population[i * gridSize + j].fitness;
         vertices[i * gridSize * 3 + j * 3] = x;
-        // console.log(fitness);
-        vertices[i * gridSize * 3 + j * 3 + 1] = fitness; // Utilisez la fitness pour la composante Y
+        vertices[i * gridSize * 3 + j * 3 + 1] += fitness * 2; // Use fitness for the Y component
         vertices[i * gridSize * 3 + j * 3 + 2] = z;
       }
     }
@@ -139,15 +179,15 @@ function displayFitnessMap(population) {
 
   const materialPoints = new THREE.PointsMaterial({
     color: 0xffffff,
-    size: 0.1,
+    size: 0.07,
   });
   materialPoints.side = THREE.DoubleSide; // to see the mesh from both ways
   const fitnessMap = new THREE.Points(geometry, materialPoints);
-  fitnessMap.castShadow = true; // Activer la projection des ombres
+  fitnessMap.castShadow = true; // Enable shadow casting
   scene.add(fitnessMap);
 
-  // Relie les points par des lignes
-  // Créez une géométrie de lignes en reliant les points adjacents dans la grille
+  // Connect the points with lines
+  // Create a lines geometry by connecting adjacent points in the grid
   const linesGeometry = new THREE.BufferGeometry();
   const linesVertices = [];
 
@@ -158,7 +198,7 @@ function displayFitnessMap(population) {
       const currentY = vertices[currentIndex * 3 + 1];
       const currentZ = vertices[currentIndex * 3 + 2];
 
-      // Ligne vers le point à droite (si disponible)
+      // Line to the right point (if available)
       if (j < gridSize - 1) {
         const rightIndex = i * gridSize + (j + 1);
         const rightX = vertices[rightIndex * 3];
@@ -169,7 +209,7 @@ function displayFitnessMap(population) {
         linesVertices.push(rightX, rightY, rightZ);
       }
 
-      // Ligne vers le point en bas (si disponible)
+      // Line to the bottom point (if available)
       if (i < gridSize - 1) {
         const bottomIndex = (i + 1) * gridSize + j;
         const bottomX = vertices[bottomIndex * 3];
@@ -181,8 +221,8 @@ function displayFitnessMap(population) {
       }
     }
 
-    // Ajout des faces reliant les lignes
-    // Créez une géométrie pour les faces entre les lignes
+    // Adding faces connecting the lines
+    // Create geometry for faces between the lines
     const facesGeometry = new THREE.BufferGeometry();
     const facesVertices = [];
 
@@ -193,7 +233,7 @@ function displayFitnessMap(population) {
         const belowIndex = (i + 1) * gridSize + j;
         const belowNextIndex = belowIndex + 1;
 
-        // Ajoutez les quatre coins du carré
+        // Add the four corners of the square
         const currentX = vertices[currentIndex * 3];
         const currentY = vertices[currentIndex * 3 + 1];
         const currentZ = vertices[currentIndex * 3 + 2];
@@ -210,15 +250,21 @@ function displayFitnessMap(population) {
         const belowNextY = vertices[belowNextIndex * 3 + 1];
         const belowNextZ = vertices[belowNextIndex * 3 + 2];
 
-        // Première face du carré
+        // First face of the square
         facesVertices.push(currentX, currentY, currentZ);
         facesVertices.push(nextX, nextY, nextZ);
         facesVertices.push(belowX, belowY, belowZ);
 
-        // Deuxième face du carré
+        // Second face of the square
         facesVertices.push(belowX, belowY, belowZ);
         facesVertices.push(nextX, nextY, nextZ);
         facesVertices.push(belowNextX, belowNextY, belowNextZ);
+
+        colors.push(
+          currentX / 30 + 0.5,
+          currentY / 30 + 0.5,
+          currentZ / 30 + 0.5
+        );
       }
     }
 
@@ -228,12 +274,13 @@ function displayFitnessMap(population) {
     );
 
     const materialFaces = new THREE.PointsMaterial({
-      color: 0x0000ff,
+      color: materialFacesColor,
       size: 0.1,
     });
+
     materialFaces.side = THREE.DoubleSide; // to see the mesh from both ways
     const fitnessFaces = new THREE.Mesh(facesGeometry, materialFaces);
-    fitnessFaces.receiveShadow = true; // Activer la réception des ombres
+    fitnessFaces.receiveShadow = true; // Enable shadow reception
     scene.add(fitnessFaces);
   }
 
@@ -241,27 +288,34 @@ function displayFitnessMap(population) {
     "position",
     new THREE.Float32BufferAttribute(linesVertices, 3)
   );
+  linesGeometry.setAttribute(
+    "color",
+    new THREE.Float32BufferAttribute(colors, 3)
+  );
 
-  const materialLines = new THREE.PointsMaterial({
-    color: 0x00ff00,
-    size: 0.1,
-  });
+  const materialLines = new THREE.LineBasicMaterial({ vertexColors: true });
   materialLines.side = THREE.DoubleSide; // to see the mesh from both ways
   const fitnessLines = new THREE.LineSegments(linesGeometry, materialLines);
   scene.add(fitnessLines);
 }
 
-// Fonction à maximiser (objectif)
+// Function to maximize (objective)
 function fitnessFunction(x) {
-  return -x * x + 4 * x; // Exemple : maximiser f(x) = -x^2 + 4x
+  // Calculate the absolute difference between x and the target value
+  const difference = Math.abs(x - targetValue);
+
+  // Calculate the fitness score by inversing the difference (the smaller the difference, the higher the score)
+  const fitnessScore = 1 / (1 + difference);
+
+  return fitnessScore;
 }
 
-// Générer une population initiale de manière aléatoire
+// Generate an initial population randomly
 function generatePopulation(popSize) {
   const population = [];
   for (let i = 0; i < popSize; i++) {
     const individual = {
-      chromosome: Math.random() * 10, // Valeur initiale aléatoire dans une plage de 0 à 10
+      chromosome: Math.random() * 10, // Random initial value within a range of 0 to 10
       fitness: 0,
     };
     population.push(individual);
@@ -269,15 +323,14 @@ function generatePopulation(popSize) {
   return population;
 }
 
-// Évaluer la fitness de chaque individu dans la population
+// Evaluate the fitness of each individual in the population
 function evaluatePopulation(population) {
   for (const individual of population) {
     individual.fitness = fitnessFunction(individual.chromosome);
-    // console.log(individual.fitness);
   }
 }
 
-// Sélectionner les individus pour la reproduction (roue de la roulette)
+// Select individuals for reproduction (roulette wheel)
 function selectParents(population) {
   const totalFitness = population.reduce(
     (sum, individual) => sum + individual.fitness,
@@ -298,42 +351,42 @@ function selectParents(population) {
   return parents;
 }
 
-// Effectuer la reproduction (croisement)
+// Perform reproduction (crossover)
 function crossover(parent1, parent2) {
-  const crossoverPoint = Math.random(); // Point de croisement entre 0 et 1
+  const crossoverPoint = Math.random(); // Crossover point between 0 and 1
   const child1 = {
     chromosome:
       parent1.chromosome * crossoverPoint +
       parent2.chromosome * (1 - crossoverPoint),
-    fitness: 0,
+    fitness: fitnessFunction(),
   };
   const child2 = {
     chromosome:
       parent2.chromosome * crossoverPoint +
       parent1.chromosome * (1 - crossoverPoint),
-    fitness: 0,
+    fitness: fitnessFunction(),
   };
   return [child1, child2];
 }
 
-// Appliquer une mutation aléatoire à un individu
+// Apply random mutation to an individual
 function mutate(individual, mutationRate) {
   if (Math.random() < mutationRate) {
-    individual.chromosome = Math.random() * 10; // Nouvelle valeur aléatoire
+    individual.chromosome = Math.random() * 10; // New random value
   }
 }
 
-// Algorithme génétique principal avec affichage du paysage adaptatif
+// Main genetic algorithm with adaptive landscape display
 function geneticAlgorithmWithAdaptiveLandscape(
   popSize,
   generations,
   mutationRate
 ) {
   let population = generatePopulation(popSize);
-  const adaptiveLandscapeInterval = Math.floor(generations / 10); // Afficher le paysage adaptatif toutes les 10 générations
+  const adaptiveLandscapeInterval = Math.floor(generations / 10); // Display adaptive landscape every 10 generations
 
   for (let gen = 0; gen < generations; gen++) {
-    evaluatePopulation(population); // Mettre à jour la fitness de la population
+    evaluatePopulation(population); // Update population fitness
     const parents = selectParents(population);
     const newPopulation = [];
 
@@ -348,28 +401,27 @@ function geneticAlgorithmWithAdaptiveLandscape(
       newPopulation.push(child1, child2);
     }
 
-    // MODIFS:
-    //population = newPopulation;
-    let populations = newPopulation;
+    population = newPopulation; // let populations = newPopulation;
 
     if (gen % adaptiveLandscapeInterval === 0) {
+      evaluatePopulation(population);
       displayFitnessMap(population);
     }
   }
 
-  // console.log(population);
-
-  // Retourner le meilleur individu de la dernière génération
   evaluatePopulation(population);
+
+  // Return the best individual from the last generation
   const bestIndividual = population.reduce(
     (best, individual) =>
       individual.fitness > best.fitness ? individual : best,
     population[0]
   );
-  return bestIndividual;
+
+  chromosomeHTML.innerHTML = bestIndividual.chromosome.toFixed(3);
+  fitnessHTML.innerHTML = bestIndividual.fitness.toFixed(3);
 }
 
-// Exemple d'utilisation de l'algorithme génétique avec affichage du paysage adaptatif
-const bestSolution = geneticAlgorithmWithAdaptiveLandscape(100, 100, 0.1);
-console.log("Meilleure solution trouvée :", bestSolution.chromosome);
-console.log("Valeur de la fonction objectif :", bestSolution.fitness);
+// Example of using the genetic algorithm with adaptive landscape display
+geneticAlgorithmWithAdaptiveLandscape(popSize, generations, mutationRate);
+targetValueHTML.innerHTML = targetValue;
